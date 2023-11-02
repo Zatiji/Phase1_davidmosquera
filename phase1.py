89828dbf1ff7fc6f0d3fe9d75e2a6ddad96fc827
David Mosquera <david.mosquera.1@ulaval.ca>
2023-11-01 20:14:17 -0400

    Création du canva

---
+++ phase1.py
@@ -0,0 +1,20 @@
+import argparse
+import datetime
+
+
+def analyser_commande():
+    """
+    Générer un interpréteur de commande.
+
+    Returns:
+        Un objet Namespace tel que retourné par parser.parse_args().
+        Cet objet aura l'attribut «symboles» représentant la liste des
+        symboles à traiter, et les attributs «début», «fin» et «valeur»
+        associés aux arguments optionnels de la ligne de commande.
+    """
+    parser = argparse.ArgumentParser()
+
+    # Complétez le code ici
+    # vous pourriez aussi avoir à ajouter des arguments dans ArgumentParser(...)
+
+    return parser.parse_args()
