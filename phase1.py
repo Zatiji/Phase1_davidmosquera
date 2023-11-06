ca359efd04d7f9a68f6392892df75d4165311b6d
David M. <103978628+Zatiji@users.noreply.github.com>
2023-11-06 13:25:04 -0500

    Implantation de la fonction analyser_commande()

--- phase1.py
+++ phase1.py
@@ -1,5 +1,6 @@
 import argparse
-import datetime
+from datetime import date
+import json
 
 
 def analyser_commande():
@@ -12,9 +13,39 @@ def analyser_commande():
         symboles à traiter, et les attributs «début», «fin» et «valeur»
         associés aux arguments optionnels de la ligne de commande.
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser("Extraction de valeurs historiques pour un ou plusieurs symboles boursiers.")
 
-    # Complétez le code ici
-    # vous pourriez aussi avoir à ajouter des arguments dans ArgumentParser(...)
+    #ajout d'argument positionnel symbole
+    parser.add_argument("symbole",
+                        nargs = "+",
+                        type = str,
+                        help = "Nom d'un symbole boursier"
+                        )
+    
+    #ajout de l'argument -d
+    parser.add_argument("-d", "--debut",
+                        dest = "debut",
+                        metavar= "DATE",
+                        help = "Date recherchée la plus ancienne (format: AAAA-MM-JJ)"
+                        )
+    
+    #ajout de l'argument -f
+    parser.add_argument("-f", "--fin",
+                        dest = "fin",
+                        metavar = "DATE",
+                        help = "Date recherchée la plus récente (format: AAAA-MM-JJ)"
+                        )
+    
+    #Ajout de l'argument -v
+    parser.add_argument("-v", "--valeur",
+                        dest = "valeur",
+                        choices=["fermeture","ouverture","min","max","volume"],
+                        default = "fermeture",
+                        help = "La valeur désirée (par défaut: fermeture)"
+                       )
+    
 
     return parser.parse_args()
+
+if __name__ == "__main__":
+    args = analyser_commande()
